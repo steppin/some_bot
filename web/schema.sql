15e60252dcc2fe75d1fb8685677ad3820627f1cf
Andrew <solzx007@umn.edu>
2014-03-31 20:53:01 -0500

    Add columns author and upload_date

--- web/schema.sql
+++ web/schema.sql
@@ -1,5 +1,7 @@
 drop table if exists maps;
 create table maps (
   id integer primary key autoincrement,
-  mapname text unique not null
+  mapname text unique not null,
+  author text,
+  upload_time text not null
 );
