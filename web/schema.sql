e7902076d6e8ae9d1e6ba9275664ce3d6debd7ed
Andrew <solzx007@umn.edu>
2014-04-04 22:00:28 -0500

    Add auto db filler scripts

--- web/schema.sql
+++ web/schema.sql
@@ -2,8 +2,15 @@ drop table if exists maps;
 create table maps (
   id integer primary key autoincrement,
   mapname text not null,
-  author text,
   upload_time real,
   times_tested integer,
   last_tested real
 );
+
+drop table if exists search;
+create virtual table search using fts4(
+	mapname,
+	author,
+	description
+);
+
