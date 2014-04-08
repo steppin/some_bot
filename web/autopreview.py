3a319cc2757f9c1d9fb24090f2437fa0283d7339
CarrotsAreMediocre <Patrick.E.Shanahan@gmail.com>
2014-04-08 08:22:15 -0500

    Brought to PEP-8 and used proper path joining.

--- web/autopreview.py
+++ web/autopreview.py
@@ -1,40 +1,40 @@
 import os
-import previewer
 import glob
-import sys
 from multiprocessing import Pool
-import itertools
+
+from previewer import previewer
 
 OUT_DIR = os.path.abspath("./static/previews/")
 MAP_DIR = os.path.abspath("./static/maps/")
 
-def generate_previews(pngpath):
-	file_name = os.path.splitext(pngpath)[0]
-	base_name = os.path.basename(pngpath)
-	jsonpath = file_name+".json"
-	print file_name
-	if not os.path.isfile(jsonpath):
-		print "Unable to find json for %s" %base_name
-		return None
 
-	try:
-		map_ = previewer.Map(pngpath, jsonpath)
-		preview = map_.preview()
-		with open(OUT_DIR+os.sep+base_name, 'wb') as f:
-			f.write(preview.getvalue())
-			print "[ ] Wrote %s successfully" %base_name
-	except:
-		print "[X] Problem with %s" %(pngpath)
-		pass
+def generate_previews(png_path):
+    file_name = os.path.splitext(png_path)[0]
+    base_name = os.path.basename(png_path)
+    json_path = file_name + ".json"
+    print file_name
+    if not os.path.isfile(json_path):
+        print "Unable to find json for %s" % base_name
+        return None
+    try:
+        map_ = previewer.Map(png_path, json_path)
+        preview = map_.preview()
+        with open(os.path.join(OUT_DIR, base_name, 'wb')) as f:
+            f.write(preview.getvalue())
+            print "[ ] Wrote {} successfully".format(base_name)
+    except:  # TODO: Super broad. Specific exceptions would be good.
+        print "[X] Problem with {}".format(png_path)
+        pass
+
 
 if __name__ == "__main__":
-	import sys
-	if len(sys.argv) != 2:
-		numprocesses = 4
-	else:
-		numprocesses = int(sys.argv[-1])
+    import sys
 
+    if len(sys.argv) != 2:
+        process_count = 4
+    else:
+        process_count = int(sys.argv[-1])
 
-	png_files = glob.glob(MAP_DIR+os.sep+"*.png")
-	p = Pool(numprocesses)
-	p.map(generate_previews, png_files)
+    png_files = glob.glob(os.path.join(MAP_DIR, "*.png"))
+    p = Pool(process_count)
+    p.map(generate_previews, png_files)
