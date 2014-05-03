2b4e5ff4f58e2284a82951a8e1692b51f02d54a2
S Teppin <super.steppin@gmail.com>
2014-05-03 00:55:28 -0400

    Add radius back to server list.

--- irc/plugins/TagproStats/plugin.py
+++ irc/plugins/TagproStats/plugin.py
@@ -39,7 +39,7 @@ from operator import itemgetter
 
 
 koala = 'http://tagpro-{0}.koalabeast.com'
-koalaservers = ['origin', 'pi', 'centra', 'sphere', 'chord', 'diameter', 'maptest', 'sector']
+koalaservers = ['origin', 'pi', 'centra', 'sphere', 'chord', 'diameter', 'maptest', 'sector', 'radius']
 jj = 'http://{0}.jukejuice.com'
 jjservers = ['tangent'] # ['secant']
 servers = [(server, koala.format(server)) for server in koalaservers] + [(server, jj.format(server)) for server in jjservers]
