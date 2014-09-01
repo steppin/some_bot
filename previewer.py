0d4ee0c67f19a3e32e73b66e1fdb6c9c0f979a91
GreenVars <so_ur_ed@yahoo.com>
2014-09-01 13:56:43 -0400

    I DID IT
    I finally read the part in my book on classes and methods and such. I went through my previous version and made it work. I'm almost completely positive this will work with no issues and will generate previews. Object-Oriented and everything. 
    CHANGES:
    Put class specific variables in the __init__
    The dictionary of cords and what sprite they are is now generated in its own function
    Changed several variable names as map was used too much
    Draw no longer needs parameters
    Took adj_walls out of the plot class 
    Added some debug data in __str__
    
    Final Concerns:
    Will the previews work in some_bot and JukeJuice?
    Shouldn't the png mode be RGB and not RGBA? 
    I think RGBA gives a 4th tuple value which messes up the program but ill leave it up to you.
    Are variables in the right scope?
    I'm horrible at github so these commit changes might look weird but who needs previous versions when this works?

--- previewer.py
+++ previewer.py
@@ -4,15 +4,48 @@ import cStringIO
 from os import listdir
 from json import load
 from PIL import Image
-map = {}
-sprites = {}
 WALLS = ["45", "135", "225", "315", "wall"]
-fails = 0
 TILE_SIZE = 40
 SIZE_LIMIT = 100*100
+# Sprite directory
+sprites = {}
+sprite_list = listdir("sprites/")
+if 'Thumbs.db' in sprite_list:
+	sprite_list.remove('Thumbs.db')
+for pic in sprite_list:
+	sprites[pic] = Image.open("sprites/"+pic)
 def usage():
-    print >> sys.stderr, 'Usage: {} PNG JSON [SPLATS] > PREVIEW'.format(
-        sys.argv[0])
+	print >> sys.stderr, 'Usage: {} PNG JSON [SPLATS] > PREVIEW'.format(
+		sys.argv[0])
+def adj_walls(x, y, paste):
+	corr_adj = {
+		"U": [(128, 112, 64), (128, 64, 112), (120, 120, 120)],
+		"R": [(128, 112, 64), (64, 128, 80), (120, 120, 120)],
+		"D": [(64, 128, 80), (64, 80, 128), (120, 120, 120)],
+		"L": [(64, 80, 128), (128, 64, 112), (120, 120, 120)]
+	}
+	adj = paste
+	# If within boundaries
+		# If wall dir corresponds
+			# Exclude wall type duplicates - Experimental 50% Success at catching odd 45s
+				# Add dir string
+	if y != 0:
+		if pixels[x,y-1] in corr_adj["U"]:
+			if ('315' and '45') not in adj:
+				adj += "U"
+	if x != self.max_x-1:
+		if pixels[x+1,y] in corr_adj["R"]:
+			if '135' not in adj:
+				adj += "R"
+	if y != self.max_y-1:
+		if pixels[x,y+1] in corr_adj["D"]:
+			if '135' not in adj:
+				adj += "D"
+	if x != 0:
+		if pixels[x-1,y] in corr_adj["L"]:
+			if '225' and '315' not in adj:
+				adj += "L"
+	return adj
 rgbs = {
 	(0, 0, 0): "black",
 	(120, 120, 120): "wall",
@@ -40,113 +73,87 @@ rgbs = {
 	(64, 80, 128): "225",
 	(128, 64, 112): "315"
 }
-# Er good class use?
-class Map():
-# Sprite directory
-    sprite_list = listdir("sprites/")
-    if 'Thumbs.db' in sprite_list:
-        sprite_list.remove('Thumbs.db')
-    for pic in sprite_list:
-        sprites[pic] = Image.open("sprites/"+pic)
-	# Odd Indentation Error
+class plot():
 	def __init__(self, pngpath, jsonpath):
 		if 'http' in pngpath:
 			png_handle = cStringIO.StringIO(urllib.urlopen(pngpath).read())
 			png = Image.open(png_handle)
 		else:
 			png = Image.open(pngpath)
+		# SHOULD THIS BE RGB------------?
 		if png.mode != 'RGBA':
 			png = png.convert('RGBA')
 		self.png = png
 		self.max_x, self.max_y = self.png.size
+		self.fails = 0
+		self.cords = map_cords()
 		if self.max_x * self.max_y > SIZE_LIMIT:
 			error_msg = "Image '{}' is too large. Limit is {} px, but it is {}x{}.".format(
 				pngpath, SIZE_LIMIT, self.max_x, self.max_y)
 			raise ValueError(error_msg)
 		self.pixels = png.load()
-		self._preview = None
-		self.portal_entrances = []
 		if 'http' in jsonpath:
 			json_file = urllib.urlopen(jsonpath)
 			self.json = load(json_file)
 		else:
 			with open(jsonpath) as fp:
 				self.json = load(fp)
-	def adj_walls(x, y, paste):
-		corr_adj = {
-			"U": [(128, 112, 64), (128, 64, 112), (120, 120, 120)],
-			"R": [(128, 112, 64), (64, 128, 80), (120, 120, 120)],
-			"D": [(64, 128, 80), (64, 80, 128), (120, 120, 120)],
-			"L": [(64, 80, 128), (128, 64, 112), (120, 120, 120)]
-		}
-		adj = paste
-		# If within boundaries
-			# If wall dir corresponds
-				# Exclude wall type duplicates - Experimental 50% Success at catching odd 45s
-					# Add dir string
-		if y != 0:
-			if pixels[x,y-1] in corr_adj["U"]:
-				if ('315' and '45') not in adj:
-					adj += "U"
-		if x != self.max_x-1:
-			if pixels[x+1,y] in corr_adj["R"]:
-				if '135' not in adj:
-					adj += "R"
-		if y != self.max_y-1: 
-			if pixels[x,y+1] in corr_adj["D"]:
-				if '135' not in adj:
-					adj += "D"
-		if x != 0:
-			if pixels[x-1,y] in corr_adj["L"]:
-				if '225' and '315' not in adj:
-					adj += "L"
-		return adj
-	def draw(pixels, data):
-		img = Image.new("RGB", (self.max_x*40,self.max_y*40), "white")
-		# xrange????
+	def __str__(self):
+		return """Map Name: {}
+		Map Author: {}
+		Map Height: {}
+		Map Width: {}
+		Tile Number: {}
+		Preview Height: {}
+		Preview Width: {}
+		Failed Displays: {}
+		""".format(self.json["info"]["name"], self.json["info"]["author"],
+		self.max_y, self.max_x, self.max_x*self.max_y, self.max_y*40, self.max_x*40, self.fails)
+	def map_cords():
+		cords = {}
 		for w in range(self.max_x):
 			for h in range(self.max_y):
-				map[(w,h)] = rgbs[pixels[w,h]]
-
-		for item in map:
-			paste = map[item]
+				cords[(w,h)] = rgbs[self.pixels[w,h]]
+		return cords
+	def draw(self):
+		img = Image.new("RGB", (self.max_x*40,self.max_y*40), "white")
+		for item in self.cords:
+			paste = self.cords[item]
 			if paste == 'gate':
 				cord_key = "%d,%d" % (item[0], item[1])
-				if cord_key in data['fields']:
-					if data['fields'][cord_key]['defaultState'] != 'off':
-						paste = data['fields'][cord_key]['defaultState']+"_gate"
+				if cord_key in self.json['fields']:
+					if self.json['fields'][cord_key]['defaultState'] != 'off':
+						paste = self.json['fields'][cord_key]['defaultState']+"_gate"
 			if paste == 'portaloff':
 				cord_key = "%d,%d" % (item[0], item[1])
 				try:
-					if "destination" in data['portals'][cord_key]:
+					if "destination" in self.json['portals'][cord_key]:
 						paste = "portal"
 				except KeyError:
 					pass
 			if paste in WALLS:
-			    # Good Function call?
 				paste = adj_walls(item[0], item[1], paste)
-			if paste+'.png' in sprites:	
+			if paste+'.png' in sprites:
 				img.paste(sprites[paste+'.png'],(item[0]*TILE_SIZE,item[1]*TILE_SIZE))
 			else:
 				fails += 1
-            if 'marsballs' in data:
-            	# xrange????
-	            for mars in range(len(data['marsballs'])):
-		        img.paste(sprites['marsball.png'],(data['marsballs'][mars]['x']*TILE_SIZE, data['marsballs'][mars]['y']*TILE_SIZE))
-        # Good return?
-        return img
+		# Add all marsballs specified in json
+		if 'marsballs' in self.json:
+			for mars in range(len(self.json['marsballs'])):
+				img.paste(sprites['marsball.png'],(self.json['marsballs'][mars]['x']*TILE_SIZE, self.json['marsballs'][mars]['y']*TILE_SIZE))
+		return img
 def main():
-    if len(sys.argv) < 3:
-        usage()
-        return 1
-    # Removed anything with splats, wasn't sure if neccessary 
-    png_path = sys.argv[1]
-    json_path = sys.argv[2]
-    # Good calls and returns?
-    map_ = Map(png_path, json_path)
-    preview = map_.draw(png_path, json_path) 
-    sys.stdout.write(preview.getvalue())
-	
+	if len(sys.argv) < 3:
+		usage()
+		return 1
+
+	png_path = sys.argv[1]
+	json_path = sys.argv[2]
+	map_ = plot(png_path, json_path)
+	preview = map_.draw()
+	# I DONT KNOW WHAT THIS DOES BUT IT LOOKS IMPORTANT
+	sys.stdout.write(preview.getvalue())
+
 if '__main__' == __name__:
-    status = main()
-    sys.exit(status)
+	status = main()
+	sys.exit(status)
