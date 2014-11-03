44710105d116680418f7e5ac4f39bf35c3403148
NewCompte <victorvvv@gmail.com>
2014-11-04 00:09:55 +0100

    Looks a bit nicer.

--- somebotweb/config.py
+++ somebotweb/config.py
@@ -1,6 +1,7 @@
 import os
-from somebotweb.test_servers import server_dict, server_list
+from somebotweb.test_servers import server_description, zone_list
 DEBUG = False
 SQLALCHEMY_DATABASE_URI = "postgres:///{}".format(os.environ.get("SOMEBOT_DB", "somebot"))
 MAX_CONTENT_LENGTH = 512 * 1024  # 512KB
-SERVER_LIST = server_list
+ZONE_LIST = zone_list
+SERVER_DESCRIPTION = server_description
--- somebotweb/test_servers.py
+++ somebotweb/test_servers.py
@@ -1,2 +1,3 @@
-server_dict = { 'eu': 'http://maptest.newcompte.fr/', 'us': 'http://tagpro-maptest.koalabeast.com/', 'ca': 'http://maptest2.newcompte.fr/', 'au': 'http://oceanic.newcompte.fr/'}
-server_list = ['us', 'ca', 'eu', 'au']
+server_url = { 'eu': 'http://maptest.newcompte.fr/', 'us': 'http://tagpro-maptest.koalabeast.com/', 'ca': 'http://maptest2.newcompte.fr/', 'au': 'http://oceanic.newcompte.fr/'}
+server_description = { 'eu': 'Paris (Fr)', 'us': 'Los Angeles', 'ca': 'Montreal (Ca)', 'au': 'Sydney (Au)'}
+zone_list = ['us', 'ca', 'eu', 'au']
