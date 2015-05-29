ab5e71f6e7b916294829cca44bfee35b57beb04b
steppin <super.steppin@gmail.com>
2015-05-28 21:53:03 -0400

    Update config.py
    Turn off DEBUG.

--- somebotweb/config.py
+++ somebotweb/config.py
@@ -1,6 +1,6 @@
 import os
 from collections import OrderedDict
-DEBUG = True
+DEBUG = False
 SQLALCHEMY_DATABASE_URI = "postgres:///{}".format(os.environ.get("SOMEBOT_DB", "somebot"))
 MAX_CONTENT_LENGTH = 512 * 1024  # 512KB
 TEST_SERVERS = OrderedDict([
